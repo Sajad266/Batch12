ece2e58ef6797781230e062a438ae2adc5d924a6
Sajad Ahmadzai <Sajad Ahmadzai@10.0.0.155>
2024-09-10 21:47:57 -0400

    added display method

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,26 +2,8 @@ package homework;
 
 public class GitHomework {
 
-	public static void main(String[] args) {
-		System.out.println("BATCH 12: Git Class Lecture");
-		int age = 12;
-		String name = "John";
-		//this is coming from github
-	}
-	
-	public static void add(int a, int b) {
-		System.out.println(a + b);
+	public void display() {
+		System.out.println("John Cena");
 		
-		//This is coming from github because Fahim said practice
-	}
-	
-	public static void add2(int c, int d) {
-		System.out.println(c + d);
-
-		//This is coming from github because Fahim said practice again
-	}
-	
-	public static void add3(int x, int y) {
-		System.out.println(x + y);
 	}
 }
