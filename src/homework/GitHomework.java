496e406490b5517fc4957508e2b8786808f2e46d
Sajad Ahmadzai <Sajad Ahmadzai@10.0.0.155>
2024-09-10 22:06:37 -0400

    empty

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,11 +2,4 @@ package homework;
 
 public class GitHomework {
 
-	public void display() {
-		System.out.println("John Cena");
-	}
-	/**
- 		this is coming from gethub
-		Where is that PILLOW full of MONEY
-	*/
 }
