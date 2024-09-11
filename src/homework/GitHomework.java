cde474ac3f0b25395110b004d8e4406db760b022
Sajad Ahmadzai <Sajad Ahmadzai@10.0.0.155>
2024-09-10 20:40:50 -0400

    other method added

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -9,4 +9,9 @@ public class GitHomework {
 	public static void add(int a, int b) {
 		System.out.println(a + b);
 	}
+	
+	public static void add2(int c, int d) {
+		System.out.println(c + d);
+	}
+
 }
